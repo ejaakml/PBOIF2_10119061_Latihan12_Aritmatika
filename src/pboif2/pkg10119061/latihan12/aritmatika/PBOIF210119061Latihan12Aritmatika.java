a478cd07927cc4da22f2446567de336b22034dc7
ejaakml <eximindotong@yahoo.com>
2020-10-20 17:51:32 +0700

    v

---
+++ src/pboif2/pkg10119061/latihan12/aritmatika/PBOIF210119061Latihan12Aritmatika.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119061.latihan12.aritmatika;
+
+/**
+ *
+ * @author Rheiza
+ */
+public class PBOIF210119061Latihan12Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b-- = " + (a--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("++d = " + (++d));
+        
+        
+    }
+    
+}
